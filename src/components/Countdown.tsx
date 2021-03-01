67b9218cc3fbdec74f899aa27ca64761e6a5b238
Carlos <carlosrocha@mecajun.com>
2021-02-28 21:33:25 -0300

    button finished countdown

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown() {
     const [time, setTime] = useState(25*60)
-    const [active, setActive] = useState(false)
+    const [isActive, setIsActive] = useState(false)
 
     const minutes = Math.floor(time/60)
     const seconds = time%60
@@ -22,16 +22,16 @@ export function Countdown() {
     const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
     function startCountdown() {
-        setActive(true)
+        setIsActive(true)
     }
 
     useEffect(()=>{
-        if(active && time){
+        if(isActive && time){
             setTimeout(()=>{
                 setTime(time-1)
             }, 1000)
         }
-    }, [active])
+    }, [isActive])
 
     return(
         <div>
@@ -41,9 +41,18 @@ export function Countdown() {
                     <span>{minuteRight}</span>
                 </div>
                 <span>:</span>
-                <div>{secondLeft}</div>
-                <div>{secondRight}</div>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
             </div>
+
+            <button
+                type="button"
+                className={styles.countdownButton}
+                onClick={startCountdown}>
+                    Iniciar um ciclo
+                </button>
         </div>
     )
 
